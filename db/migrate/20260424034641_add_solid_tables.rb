dad9000771716ecd460469df843fa8b32b56a0ba
ndikrp <andhikarizky36@gmail.com>
2026-04-24 13:08:11 +0700

    fix: linting error on cicd

--- db/migrate/20260424034641_add_solid_tables.rb
+++ db/migrate/20260424034641_add_solid_tables.rb
@@ -6,9 +6,9 @@ class AddSolidTables < ActiveRecord::Migration[7.1]
         t.binary "payload", limit: 536870912, null: false
         t.datetime "created_at", null: false
         t.integer "channel_hash", limit: 8, null: false
-        t.index ["channel"], name: "index_solid_cable_messages_on_channel"
-        t.index ["channel_hash"], name: "index_solid_cable_messages_on_channel_hash"
-        t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
+        t.index [ "channel" ], name: "index_solid_cable_messages_on_channel"
+        t.index [ "channel_hash" ], name: "index_solid_cable_messages_on_channel_hash"
+        t.index [ "created_at" ], name: "index_solid_cable_messages_on_created_at"
       end
     end
 
@@ -19,9 +19,9 @@ class AddSolidTables < ActiveRecord::Migration[7.1]
         t.datetime "created_at", null: false
         t.integer "key_hash", limit: 8, null: false
         t.integer "byte_size", limit: 4, null: false
-        t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
-        t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
-        t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
+        t.index [ "byte_size" ], name: "index_solid_cache_entries_on_byte_size"
+        t.index [ "key_hash", "byte_size" ], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+        t.index [ "key_hash" ], name: "index_solid_cache_entries_on_key_hash", unique: true
       end
     end
   end
